92c3ad8e80f696815cb391f11deb82fc3c908346
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2022-06-26 00:42:14 +0600

    blueprint create & register for admin

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,13 @@
+import os
 from flask import Flask
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'bangla bully detection using machine learning'
+    
+    from .admin import admin
+
+    app.register_blueprint(admin, url_prefix='/admin')
 
     return app
