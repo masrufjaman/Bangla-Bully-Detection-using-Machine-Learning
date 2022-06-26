2eda82cf5ed350e1669c1319dded0fa0f8466780
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2022-06-27 00:57:12 +0600

    start admin dashboard

--- website/admin.py
+++ website/admin.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 admin = Blueprint('admin', __name__)
 
 
 @admin.route('/')
 def admin_dashboard():
-    return "<h1>Admin Dashboard</h1>"
+    return render_template("admin_dashboard.html")
