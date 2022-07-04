c0b014785d74971f13fef7bffdff44d5e59a966b
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2022-07-04 10:25:23 +0600

    create view for help page

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
@@ -6,3 +6,8 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return "<h1>Welcome to homepage.</h>"
+
+@views.route('/help')
+def help():
+    return "<h1>Welcome to help page.</h>"
+    # return render_template("html_file_name.html")   <--- after you create an html page for help webpage
