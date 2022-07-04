8d07320994ad659a83079fa2fa4185d7f1ce3ed9
Nazifa Nahian <nazifa.nahian@northsouth.edu>
2022-07-05 01:51:35 +0600

    login html,css(half)

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
@@ -6,3 +6,7 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return "<h1>Welcome to homepage.</h>"
+
+@views.route('/login')
+def login():
+    return render_template("login.html")
