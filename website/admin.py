787476cf9bfbe0178c7b5785343c46ce407558e8
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2022-07-06 15:45:45 +0600

    customers info webpage

--- website/admin.py
+++ website/admin.py
@@ -6,3 +6,7 @@ admin = Blueprint('admin', __name__)
 @admin.route('/')
 def admin_dashboard():
     return render_template("admin_dashboard.html")
+
+@admin.route('/customers-info')
+def customers_info():
+    return render_template("customers_info.html")
