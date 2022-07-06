e48350c188649668b04d0a8c8ba95f721d283feb
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2022-07-06 16:45:54 +0600

    update view.py

--- website/views.py
+++ website/views.py
@@ -1,31 +1,33 @@
 from flask import Blueprint, render_template
 
-views = Blueprint('views', __name__)
+views = Blueprint("views", __name__)
 
 
-@views.route('/')
+@views.route("/")
 def home():
     return render_template("homepage.html")
 
-@views.route('/login')
+
+@views.route("/login")
 def login():
     return render_template("login.html")
 
-@views.route('/ResetPass')
+
+@views.route("/ResetPass")
 def reset_pass():
     return render_template("ResetPass.html")
 
-@views.route('/SignUp')
+
+@views.route("/SignUp")
 def SignUp():
     return render_template("SignUp.html")
 
-@views.route('/help')
-def help():
-    # return "<h1>Welcome to help page.</h>"
-    return render_template("help.html")
-    #<--- after you create an html page for help webpage
 
-@views.route('/profile')
+@views.route("/help")
+def help():
+    return render_template("help.html")
+
+
+@views.route("/profile")
 def profile():
-    # return "<h1>Welcome to profile page.</h>"
     return render_template("profile.html")
