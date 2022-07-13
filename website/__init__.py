2c23c0873d9c24d7f7f1716a62832e985c9547f6
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2022-07-14 02:54:11 +0600

    connect with mysql

--- website/__init__.py
+++ website/__init__.py
@@ -1,15 +1,22 @@
-import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'bangla bully detection using machine learning'
-    
+    app.config["SECRET_KEY"] = "bangla bully detection using machine learning"
+    # for database
+    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:@127.0.0.1/snooper"
+    db.init_app(app)
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
     from .admin import admin
     from .views import views
 
-    app.register_blueprint(admin, url_prefix='/admin')
-    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(admin, url_prefix="/admin")
+    app.register_blueprint(views, url_prefix="/")
 
     return app
