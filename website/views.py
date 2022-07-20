ba2fa10db76cf3c07fd8144c356dc4f186864ae1
Nazifa Nahian <nazifa.nahian@northsouth.edu>
2022-07-20 15:10:07 +0600

    upload edit

--- website/views.py
+++ website/views.py
@@ -31,9 +31,10 @@ def home():
     if file and allowed_file(file.filename):
        filename = secure_filename(file.filename)
        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
- return render_template("homepage.html", filename="filename")
+       print("File saved")
+ return render_template("homepage.html")
+    
    
-
 @views.route("/login")
 def login():
     return render_template("login.html")
