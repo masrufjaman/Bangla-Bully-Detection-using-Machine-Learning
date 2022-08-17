2dcf1733273d283ba8cef17aa413e7d890c19f65
Nazifa Nahian <nazifa.nahian@northsouth.edu>
2022-08-17 15:38:25 +0600

    database

--- website/models.py
+++ website/models.py
@@ -3,8 +3,12 @@ from . import db
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer , primary_key = True)
+    id = db.Column(db.Integer, primary_key = True)
     first_name = db.Column(db.String(150))
     last_name = db.Column(db.String(150))
     email = db.Column(db.String(150),unique = True)
-    password = db.Column(db.String(150))
+    phone_number = db.Column(db.String(11))
+    password = db.Column(db.String(8))
+    occupation = db.Column(db.String(10))
+    date_of_birth =db.Column(db.Date)
+    gender = db.Column(db.String(20))
