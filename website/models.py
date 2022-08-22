8a3f785d657ee26c4c7b45cb45d941af6d72e6de
RaOishi19 <rubaiyet.oishi@northsouth.edu>
2022-08-22 13:22:42 +0600

    Unique value change

--- website/models.py
+++ website/models.py
@@ -4,7 +4,7 @@ from . import db
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer,autoincrement = True, primary_key = True)
+    id = db.Column(db.Integer,autoincrement = True, primary_key = True, unique=True)
     first_name = db.Column(db.String(150))
     last_name = db.Column(db.String(150))
     email = db.Column(db.String(150),unique = True)
